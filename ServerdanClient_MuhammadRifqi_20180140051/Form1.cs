10356ebd2b7e030a8058239d3d2ea580f631d3b6
RifqiFirmasyah <56532181+UB8051@users.noreply.github.com>
2020-10-31 15:13:11 +0700

    Add project files.

---
+++ ServerdanClient_MuhammadRifqi_20180140051/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using ServiceMtk2_P2_20180140051;
+using static ServerdanClient_MuhammadRifqi_20180140051.Program;
+
+namespace ServerdanClient_MuhammadRifqi_20180140051
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            label3.Text = "Klik OFF Untuk Mematikan Server";
+            label2.Text = "Server ON";
+
+            Server server = new Server();
+            server.OnServer();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            label3.Text = "Klik ON Untuk Menjalankan Server";
+            label2.Text = "Server OFF";
+
+            Server server = new Server();
+            server.OffServer();
+        }
+    }
+}
---
+++ ServerdanClient_MuhammadRifqi_20180140051/Program.cs
@@ -0,0 +1,65 @@
+﻿using ServiceMtk2_P2_20180140051;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerdanClient_MuhammadRifqi_20180140051
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+        public class Server
+        {
+            public void OnServer()
+            {
+                ServiceHost hostObj = null;
+                try
+                {
+                    hostObj = new ServiceHost(typeof(Matematika));
+                    hostObj.Open();
+                    Console.WriteLine("Server is Ready!!!");
+                    Console.ReadLine();
+                    hostObj.Close();
+                }
+                catch (Exception ex)
+                {
+                    hostObj = null;
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                }
+            }
+            public void OffServer()
+            {
+                ServiceHost hostObj = null;
+                try
+                {
+                    hostObj = new ServiceHost(typeof(Matematika));
+                    hostObj.Open();
+                    Console.WriteLine("Server is Off!!!");
+                    Console.ReadLine();
+                    hostObj.Close();
+                }
+                catch (Exception ex)
+                {
+                    hostObj = null;
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
